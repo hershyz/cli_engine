36fc79720c8c8e50fb15c3a60dbd83753caad550
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-03-18 15:49:26 -0400

    Update main.cpp
    Added raw command input and parsing into command and arg.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 //Standard
 #include <iostream>
+#include <fstream>
+#include <string>
 
 //Imports:
 #include "IO.h"
@@ -14,5 +16,28 @@ IO io;
 Logger logger;
 
 int main() {
+
     logger.init(); //Shows welcome message on program start.
+
+    while (true) {
+        
+        string rawCommand = logger.input();
+        istringstream stm(rawCommand);
+
+        string command;
+        string arg1;
+        int i = 0;
+
+        //Parses raw input into a command and argument:
+        while (i <= 1) {
+            if (i == 0) {
+                stm >> command;
+            }
+            if (i == 1) {
+                stm >> arg1;
+            }
+
+            i++;
+        }
+    }
 }
