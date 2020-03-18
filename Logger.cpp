cccc8afb08dd4dca0d19f60e08715192d2f14be1
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-03-17 20:52:26 -0400

    Update Logger.cpp
    add init function

--- Logger.cpp
+++ Logger.cpp
@@ -1,9 +1,19 @@
 #include "Logger.h"
 #include <iostream>
+#include "Properties.h"
+#include "Properties.cpp"
+using namespace std;
+
+Properties properties;
 
 //Constructor:
 Logger::Logger() {
 
+}
+
+//Init:
+void Logger::init() {
+    cout << "localsrc - " + properties.version << endl;
 }
 
 //Help:
